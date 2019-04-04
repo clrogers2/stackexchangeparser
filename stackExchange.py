4ac925b107d57e4842b2e6c85462040014fce3c0
Christopher Rogers <christopher-rogers@pluralsight.com>
2019-04-04 13:12:30 -0600

    PEP 8 refactor, additional documentation. __iter__ properly calls log()

--- stackExchange.py
+++ stackExchange.py
@@ -3,7 +3,7 @@ import html
 from html.parser import HTMLParser
 from xml.etree import ElementTree as ET
 from pathlib import Path
-from prodigy.utils import log
+from prodigy import log
 
 
 class StackExchange(object):
@@ -19,10 +19,13 @@ class StackExchange(object):
             self.strict = False
             self.convert_charrefs= True
             self.fed = []
+
         def handle_data(self, d):
             self.fed.append(d)
+
         def get_data(self):
             return ''.join(self.fed)
+
         def error(self, message):
             pass
     
@@ -39,6 +42,8 @@ class StackExchange(object):
         
         # Check that the path actually exists and is a recognizable XML file
         se_file = Path(file).absolute()
+        # There is no easy way to stream into memory, Archives and files compressed using 7zip's LZMA format
+        # So we must rely on the user to unzip the archive before using the file.
         assert (se_file.exists()), "Cannot find file. Please check the path name and try again"
         assert (se_file.suffix == '.xml'), "File does not end in '.xml'. Please check the path name and try again"
         log('STREAM: {} file found'.format(se_file.as_posix()))
@@ -47,7 +52,8 @@ class StackExchange(object):
         # Regex to find newlines
         self.newlines = newlines
         self.newline = re.compile(r'\n+')
-        #If user doesn't supply the community assume a normal unziping process occured and the community is the parent directory
+        # If user doesn't supply the community assume a normal unziping process occured and
+        # the community is the parent directory
         if not community:
              community = self.file.parent.parts[-1]
         self.community = community
@@ -61,7 +67,8 @@ class StackExchange(object):
         # TODO: Change to 'end' events only
         self.tree = ET.iterparse(self.file.as_posix(), events=['start', 'end'])
         
-        #To identify even more specific results a user can supply a StackExchange tag or tags. Only Posts with one or more tags will be returned
+        # To identify even more specific results a user can supply a StackExchange tag or tags.
+        # Only Posts with one or more tags will be returned
         if type(onlytags) == str:
             onlytags = [onlytags]
         self.onlytags = onlytags
@@ -73,8 +80,7 @@ class StackExchange(object):
         # Keep track of Question tags for use by Answer Posts
         # Also keep a count of the number of expected answers and the number of seen answers
         self.parent_post_tags = {}
-        
-    
+
     def _parse_tags(self, tags):
         """
         Parse the Tags attribute of a row in a StackExchange Posts xml file
@@ -96,27 +102,29 @@ class StackExchange(object):
         for _, child in self.tree:
             
             self.total += 1
-            log("STREAM: Fetching {} child element".format(self.total, child.attrib)
+            log("STREAM: Fetching {} child element".format(self.total, child.attrib))
             # Start of file, check that the file matches the expected content_type
             if _ == 'start' and child.tag != 'row':
                 if self.content_type in self._TYPES[:3]:
-                    assert(child.tag == 'posts'), "Input file is not a StackExchange Posts.xml file. Please check the path name and try again"
+                    assert(child.tag == 'posts'), "Input file is not a StackExchange Posts.xml file. \
+                    Please check the path name and try again"
 
                 else:
-                    assert(child.tag == 'comments'), "Input file is not a StackExchange Comments.xml file. Please check the path name and try again"
+                    assert(child.tag == 'comments'), "Input file is not a StackExchange Comments.xml file. \
+                    Please check the path name and try again"
 
             else:   
-                 # Assemble the prodigy stream compliant dictonary object
+                # Assemble the prodigy stream compliant dictonary object
                 info = {"meta": {"source": "StackExchange", "Community": self.community, "type": self.file.stem}}
 
                 atb = child.attrib
-                #Fetch the necessary information 
+                # Fetch the necessary information
                 if self.content_type in self._TYPES[:3]:
                     Id = atb.get('Id', None)
                     title = atb.get('Title', None)
                     body = atb.get('Body', None)
                     tags = atb.get('Tags', None)
-                    if tags != None:
+                    if tags is not None:
                         tags = self._parse_tags(tags)
                     posttype = atb.get('PostTypeId', None)
                     create = atb.get('CreationDate', None)
@@ -137,7 +145,8 @@ class StackExchange(object):
                         else:
                             tags = None
                         
-                        #If a parent answer value is found, update the seen answer count and check if all answers have been seen
+                        # If a parent answer value is found, update the seen answer count and
+                        # check if all answers have been seen
                         if parent:
                                                     
                             # TODO: Test this code to verify it works as intended
@@ -145,13 +154,15 @@ class StackExchange(object):
                             if parent['seen'] < parent['answers']:
                                 parent['seen'] += 1
 
-                            # Now check if we've seen all the answers, If so, then delete the Parent Id entry to free up memory
+                            # Now check if we've seen all the answers,
+                            # If so, then delete the Parent Id entry to free up memory
                             if parent['seen'] == parent['answers']:
                                 del self.parent_post_tags[parentid]    
                             
                     
-                    # If the user only wants text from Posts with specific stackExchange tags, only return content that matches
-                    # Naively iterates through the stream of Posts. It does not know if the tag actually exists
+                    # If the user only wants text from Posts with specific stackExchange tags,
+                    # only return content that matches. Naively iterates through the stream of Posts.
+                    # It does not know if the tag actually exists.
                     # If the user supplies tags, and no tags match, skip this post
                     if self.onlytags and not tags:
                         continue
@@ -161,7 +172,7 @@ class StackExchange(object):
                         
                         if self.content_type == 'post_both':
                             if title and body:
-                                text =  title + '\n' + body 
+                                text = title + '\n' + body
                             elif body and not title:
                                 text = body
                             elif title and not body:
@@ -184,17 +195,18 @@ class StackExchange(object):
                     text = None
 
                 # Check to see if valid text was found, if not, skip to the next xml child element
-                if not text:
+                if text is None:
                     continue
 
                 else:
                     # unescape HTML encoding and remove html tags
-                    #Preserve the original HTML
+                    # Preserve the original HTML
                     info['html'] = text
+
+                    # TODO: Sometimes causes problems in the HTML stripper, disable for now, investigate later
+                    #text = html.unescape(text)
                     
-                    #text = html.unescape(text)  #TODO: Sometimes causes problems in the HTML stripper, disable for now, investigate later
-                    
-                    #HTML Stripper
+                    # HTML Stripper
                     stripper = self.__MLStripper__()
                     stripper.feed(text)
                     text = stripper.get_data()
@@ -211,7 +223,7 @@ class StackExchange(object):
                     info['meta']['Date'] = create
                     info['meta']['Tags'] = tags
 
-                    #yield the dictionary
+                    # yield the dictionary
                     self.parsed += 1
                     log("STREAM: {p} of {t} XML child element parsed".format(p=self.parsed, t=self.total), info)
                     yield info
